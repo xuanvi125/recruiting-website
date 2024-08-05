7e223f433a4c07f31495117a8a65bbc7f964095d
xuanvi <nguyenxuanvi770@gmail.com>
2024-08-05 07:39:15 +0700

    init project

---
+++ src/main/java/com/bugboo/CareerConnect/CareerConnectApplication.java
@@ -0,0 +1,13 @@
+package com.bugboo.CareerConnect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CareerConnectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CareerConnectApplication.class, args);
+	}
+
+}
